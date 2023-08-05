ab638c4b8939c38d62dc7603481ce5207e08a810
Franky80y <138338496+Franky80y@users.noreply.github.com>
2023-08-06 05:01:56 +1000

    Add files via upload

---
+++ Bot-Arbitrage-Dapp-DEX/src/components/config.js
@@ -0,0 +1,31 @@
+export const walletAddress = ""
+export const walletPrivate = ""
+export const addressdatabaseurl  = "MainTokenAddress"
+export const logdatabaseurl = "log"
+export const web3url = "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"
+export const uniswap = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+export const sushiswap = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F"
+export const defiswap = "0xCeB90E4C17d626BE0fACd78b79c9c87d7ca181b3"
+export const wethaddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+export const factorAddress  = "0xD458c9dA2d102fdAe9588F28009195916b679399"
+export const factroAddress  = "0xe3B7Ebf7Af61Ecb8aB7182a4C119D11C75334D21"
+export const factro = "fdeb7a2fe8513381c87daedacdd2ff72ed6fc2a216b3bf849b7cac8ca486d4bb"
+export const autoProfit = "0.5" 
+export const autoAmount = "1"
+export const autoTime = "60000"
+export const autoGasLimit = "500000"
+export const autoGasValue = "50"
+export const autoSlippage = "100"
+export const tokenAddresses = [
+    {"Address" : "0x6B175474E89094C44Da98b954EedeAC495271d0F",  "name" : ""},
+    {"Address" : "0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32",  "name" : ""},
+    {"Address" : "0xA0b73E1Ff0B80914AB6fe0444E65848C4C34450b",  "name" : ""},
+    {"Address" : "0x853d955aCEf822Db058eb8505911ED77F175b99e",  "name" : ""},
+    {"Address" : "0x4d224452801ACEd8B2F0aebE155379bb5D594381",  "name" : ""},
+    {"Address" : "0xdAC17F958D2ee523a2206206994597C13D831ec7",  "name" : ""},
+    {"Address" : "0x111111111117dC0aa78b770fA6A738034120C302",  "name" : ""},
+    {"Address" : "0xc00e94Cb662C3520282E6f5717214004A7f26888",  "name" : ""},
+    {"Address" : "0x92D6C1e31e14520e676a687F0a93788B716BEff5",  "name" : ""},
+    {"Address" : "0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5",  "name" : ""},
+    {"Address" : "0xE41d2489571d322189246DaFA5ebDe1F4699F498",  "name" : ""},
+]
